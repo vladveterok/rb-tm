6ec35ff154843d7c72134cd153f2315b2253fde2
vladveterok <vl.berezhnoi@gmail.com>
2020-02-28 16:21:18 +0200

    migrated projects and tasks into schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+Project.destroy_all
+User.destroy_all
+
+@vlad = User.create!([
+  {
+    email: 'vl.berezhnoi@gmail.com',
+    password: '12345678',
+    password_confirmation: '12345678'
+  }
+])
+
+p "Created #{User.count} user!"
+
+projects = [
+  {
+    name: 'My first Project',
+    user_id: User.first.id
+  },
+
+  {
+    name: 'My second Project',
+    user_id: User.first.id
+  },
+
+  {
+    name: 'My third Project',
+    user_id: User.first.id
+  }
+]
+
+Project.create!(projects)
+p "Created #{Project.count} projects!"
+
+tasks = [
+  {
+    name: 'My first task for first project',
+    status: false,
+    position: 01,
+    project_id: Project.first.id
+  },
+
+  {
+    name: 'My second task for first project',
+    status: false,
+    position: 02,
+    project_id: Project.first.id
+  },
+
+  {
+    name: 'My third task for first project',
+    status: false,
+    position: 03,
+    project_id: Project.first.id
+  },
+]
+
+Task.create!(tasks)
+p "Created #{Task.count} tasks!"
