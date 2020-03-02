c825d61cd4459cd329046775b0bddab62ed015ca
vladveterok <vl.berezhnoi@gmail.com>
2020-03-02 20:41:04 +0200

    added: editing projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,8 +7,8 @@ class ProjectsController < ApplicationController
   # GET /projects.json
   def index
     @projects = current_user.projects.order(created_at: :desc).all
-    @project = Project.new
-    @new_task = Task.new
+    #@new_project = Project.new
+    #@new_task = Task.new
   end
 
   # GET /projects/1
@@ -27,32 +27,24 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    #@project = Project.new(project_params)
 
-    unless @project.save
-      head :unprocessable_entity
-    end
+    #unless @project.save
+    #  head :unprocessable_entity
+    #end
   end
 
   # PATCH/PUT /projects/1
   # PATCH/PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Yay! Project was so updated!' }
-        format.js
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update(project_params)
+      render "show"
     end
   end
 
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    #@project.tasks.destroy
     @project.destroy
   end
 
