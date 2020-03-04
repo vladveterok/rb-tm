3ee4753765f7bbd2df6646f465578f269e641597
vladveterok <vl.berezhnoi@gmail.com>
2020-03-04 17:05:44 +0200

    added: default name for new projects; project validation by name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,14 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
-  #validates :name, presence: true
+  validates :name, presence: true
+  
+  after_initialize :set_defaults
+  
+def set_defaults
+  self.name ||= "New project"
+end
+
   # -> { order(:position) },
   #Project.order(created_at: :asc)
 end
